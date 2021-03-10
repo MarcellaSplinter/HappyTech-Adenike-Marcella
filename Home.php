f58c0172e0ea816cd499ee90b5f3afad0dad70c8
Herdeynikeh <78571409+Herdeynikeh@users.noreply.github.com>
2021-03-10 10:37:55 +0000

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel ="stylesheet" href ="./Feedback.css">
+</head>
+<body>
+<header>
+        
+            <h1>HappyTech Technology Company</h1>
+            
+                <div class="container">
+
+            <div id="Title">
+                <h3>Candidates Feedback Application</h3>
+            </div>
+                
+                    <nav>
+                        <ul>
+                    <li class="current"><a href="Home.php">Home</a></li>
+                <li><a href="templates.php">Templates</a></li>
+
+                <li><a href="Feedback.php">Generate Feedback</a></li>
+                <li><a href="Savedfeedback.php">Saved Feedback</a></li>
+                <li><a href="SentFeedback.php">Sent Feedback</a></li>
+                <li><a href="Delete.php">Deleted Feedback</a></li>
+
+                        </ul>
+                    </nav>
+                </div>
+            
+            </header>
+</body>
+</html>
