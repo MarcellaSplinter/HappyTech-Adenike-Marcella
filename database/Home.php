478b8828693702b0bcff21143670905cb4718bc7
MarcellaSplinter <78511276+MarcellaSplinter@users.noreply.github.com>
2021-03-10 11:53:29 +0100

    Create Home.php

---
+++ database/Home.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'common.php';
+?>
+                <div class="container">
+
+            <div id="Title">
+                <h3>Candidates Feedback Application</h3>
+                
+            </div>
+                
+                    <nav>
+                        <ul>
+                    <li class="current"><a href="Home.php">Home</a></li>
+                <li><a href="Template.php">Templates</a></li>
+
+                <li><a href="GenerateFeedback.html">Generate Feedback</a></li>
+                <li><a href="SavedFeedback.html">Saved Feedback</a></li>
+                <li><a href="SentFeedback.html">Sent Feedback</a></li>
+                <li><a href="DeletedFeedback.html">Deleted Feedback</a></li>
+
+                        </ul>
+                    </nav>
+                </div>
+            
+            </header>
+
+        
+    
+    
+    
+</body>
+</html>
